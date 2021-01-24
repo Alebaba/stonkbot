fe2f9dbc66343272a4aef3b3fc1d1c799043e947
Aleksi Järvelä <aleksi.jarvela@gmail.com>
2021-01-24 05:01:44 +0200

    Prevent adding duplicate tickers

--- stonkbot/dbhelper.py
+++ stonkbot/dbhelper.py
@@ -9,15 +9,18 @@ class DBHelper:
         self.conn.close()
 
     def setup(self):
-        stmt = "CREATE TABLE IF NOT EXISTS tickers (owner text, ticker text)"
+        stmt = "CREATE TABLE IF NOT EXISTS tickers (owner TEXT, ticker TEXT)"
         self.conn.execute(stmt)
         self.conn.commit()
 
     def add_ticker(self, owner, ticker):
-        stmt = "INSERT INTO tickers (owner, ticker) VALUES (?, ?)"
+        stmt = 'INSERT INTO tickers (owner, ticker)\
+            SELECT ?1, ?2\
+            WHERE NOT EXISTS (SELECT 1 FROM tickers WHERE owner = ?1 AND ticker = ?2)'
         args = (owner, ticker)
         self.conn.execute(stmt, args)
         self.conn.commit()
+
 
     def delete_ticker(self, owner, ticker):
         stmt = "DELETE FROM tickers WHERE (owner, ticker) = (?, ?)"
