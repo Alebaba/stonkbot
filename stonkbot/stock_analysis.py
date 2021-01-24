2b0ac0f3aeee3f40a7678c5ec61464d0084a45d7
Aleksi Järvelä <aleksi.jarvela@gmail.com>
2021-01-24 16:56:56 +0200

    Reduce api key usage and add some checks

--- stonkbot/stock_analysis.py
+++ stonkbot/stock_analysis.py
@@ -1,8 +1,10 @@
+from datetime import date
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 from api_keys import ALPHA_VANTAGE_API_KEYS
 
 # Alpha vantage limitations: 5 API requests per minute and 500 requests per day
+# Date format: YYYY-MM-DD
 
 class TechnicalAnalysis:
     def __init__(self):
@@ -13,6 +15,7 @@ class TechnicalAnalysis:
         self.tidatas = {}
         self.bbdatas = {}
         self.rsidatas = {}
+        self.currentdate = date.today().strftime('%Y-%m-%d')
     
     # Calling this method will use 1 API request
     def get_price_data(self, ticker, timeframe='daily'):
@@ -26,28 +29,26 @@ class TechnicalAnalysis:
     def get_technical_indicators(self, ticker):
         self.tidatas[ticker] = TechIndicators(key=self.currentapikey, output_format='pandas')
      
-    # Calling this method will use 1 API request
-    # TODO - Check for updated data
+    # Calling this method may use 1 API request
     def get_bollinger_band_data(self, ticker):
-        if ticker not in self.bbdatas:
+        self.update_date()
+        if (ticker not in self.bbdatas) or (self.currentdate not in self.bbdatas[ticker]):
             self.bbdatas[ticker], metadata = self.tidatas[ticker].get_bbands(symbol=ticker, interval='daily', time_period=20)
         return self.bbdatas[ticker]
 
-     # Calling this method will use 1 API request
-     # TODO - Check for updated data
+     # Calling this method may use 1 API request
     def get_rsi_data(self, ticker):
-        if ticker not in self.rsidatas:
+        self.update_date()
+        if (ticker not in self.rsidatas) or (self.currentdate not in self.rsidatas[ticker]):
             self.rsidatas[ticker], metadata = self.tidatas[ticker].get_rsi(symbol=ticker, interval='daily', time_period=20)
         return self.rsidatas[ticker]
      
     # Bollinger band is considered to be crossed, if close or open is over the the upper or lower band
     # Calling this method may use 1 API request
-    # Date format: YYYY-MM-DD
-    # TODO - add checks for dates that doesn't exist in the tables
     def bollinger_band_crossed(self, ticker, date):
         self.get_bollinger_band_data(ticker=ticker)
         
-        try:
+        if date in self.bbdatas[ticker]:
             lastdaydata_bb = self.bbdatas[ticker][date]
             lastdayopen = self.pricedatas[ticker][0][date]['1. open'][0]
             lastdayclose = self.pricedatas[ticker][0][date]['4. close'][0]
@@ -66,20 +67,25 @@ class TechnicalAnalysis:
                 return [True, 'Lower band crossed']
             else:
                 return [False, 'No band crossed']
-        except ZeroDivisionError:
+        else:
             return [False, 'No data for a given day']
     
     # Calling this method may use 1 API request
-    # TODO - add checks for dates that doesn't exist in the tables
     def rsi_limit_crossed(self, ticker, date):
         self.get_rsi_data(ticker)
         
-        if (self.rsidatas[ticker]['RSI'][date] > 70):
-            return [True, 'RSI is over 70']
-        elif (self.rsidatas[ticker]['RSI'][date] < 30):
-            return [True, 'RSI is under 30']
+        if date in self.rsidatas[ticker]['RSI']:
+            if (self.rsidatas[ticker]['RSI'][date] > 70):
+                return [True, 'RSI is over 70']
+            elif (self.rsidatas[ticker]['RSI'][date] < 30):
+                return [True, 'RSI is under 30']
+            else:
+                return [False, 'RSI is between 30 and 70']
         else:
-            return [False, 'RSI is between 30 and 70']
+            return [False, 'No data for a given day']
+
+    def update_date(self):
+        self.currentdate = date.today().strftime('%Y-%m-%d')
 
 # TODO - fundamentals
 import yfinance as yf
